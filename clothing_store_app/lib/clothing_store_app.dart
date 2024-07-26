89f613563e127718d40245365f40e2e440e7c664
Nam PHAM <127522074+hhannaap@users.noreply.github.com>
2024-07-26 22:01:41 +0700

    add address model & firebase data

--- clothing_store_app/lib/clothing_store_app.dart
+++ clothing_store_app/lib/clothing_store_app.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:clothing_store_app/modules/Address/address_list_screen.dart';
+import 'package:clothing_store_app/modules/ShoppingCart/editable-address-list-screen.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -14,6 +15,7 @@ import 'languages/appLocalizations.dart';
 import 'modules/Address/address_list_screen.dart';
 import 'modules/Address/edit_address_screen.dart';
 import 'modules/Address/add_new_address_screen.dart';
+import 'modules/ShoppingCart/editable-address-list-screen.dart';
 import 'routes/routes_name.dart';
 
 BuildContext? applicationcontext;
@@ -75,7 +77,7 @@ class _ClothingStoreAppState extends State<ClothingStoreApp> {
 
   Map<String, WidgetBuilder> _buildRoutes() {
     return {
-      RoutesName.splashScreen: (context) => AddNewAddressScreen(),
+      RoutesName.splashScreen: (context) => EditableAddressListScreen(),
     };
   }
 
--- clothing_store_app/lib/modules/Address/edit_address_screen.dart
+++ clothing_store_app/lib/modules/Address/edit_address_screen.dart
@@ -1,54 +1,165 @@
+import 'package:clothing_store_app/widgets/common_textfield.dart';
+import 'package:clothing_store_app/widgets/common_button.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:dropdown_search/dropdown_search.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter/services.dart'; // Import for TextInputFormatter
+import '../../providers/address_model.dart';
 
 class EditAddressScreen extends StatefulWidget {
-  const EditAddressScreen({super.key});
+  final String addressId;
+  final Map<String, dynamic> addressData;
+
+  EditAddressScreen({required this.addressData, required this.addressId});
 
   @override
   _EditAddressScreenState createState() => _EditAddressScreenState();
 }
 
 class _EditAddressScreenState extends State<EditAddressScreen> {
-  String? selectedProvince;
-  String? selectedDistrict;
-  String? selectedWard;
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
 
-  List<String> provinces = List.generate(100, (index) => "Province $index");
-  List<String> districts = ["District 1", "District 2", "District 3"];
-  List<String> wards = ["Ward 1", "Ward 2", "Ward 3"];
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   final addressModel = Provider.of<AddressModel>(context, listen: false);
+  //   addressModel.setName(widget.addressData['name']);
+  //   addressModel.setPhone(widget.addressData['phone']);
+  //   addressModel.setAddress(widget.addressData['address']);
+  //   addressModel.setSelectedProvinceCode(widget.addressData['province']);
+  //   addressModel.setSelectedDistrictCode(widget.addressData['district']);
+  //   addressModel.setSelectedWardCode(widget.addressData['ward']);
 
-  void _useCurrentLocation() {
-    // Add your location fetching logic here
-    print('Fetching current location...');
+  //   nameController.text = widget.addressData['name'] ?? '';
+  //   phoneController.text = widget.addressData['phone'] ?? '';
+  //   addressController.text = widget.addressData['address'] ?? '';
+  // }
+
+  Future<List<String>> _getProvinces(String query) async {
+    List<String> provinces = [];
+    QuerySnapshot snapshot = await firestore.collection('provinces').get();
+    for (var doc in snapshot.docs) {
+      provinces.add(doc['name']);
+    }
+    return provinces;
+  }
+
+  Future<String?> _getProvinceCode(String provinceName) async {
+    QuerySnapshot snapshot = await firestore
+        .collection('provinces')
+        .where('name', isEqualTo: provinceName)
+        .get();
+    return snapshot.docs.isNotEmpty ? snapshot.docs[0]['code'] : null;
+  }
+
+  Future<List<String>> _getDistricts(String query) async {
+    List<String> districts = [];
+    final addressModel = Provider.of<AddressModel>(context, listen: false);
+    QuerySnapshot snapshot = await firestore
+        .collection('districts')
+        .where('province_code', isEqualTo: addressModel.selectedProvinceCode)
+        .get();
+    for (var doc in snapshot.docs) {
+      districts.add(doc['name']);
+    }
+    return districts;
+  }
+
+  Future<String?> _getDistrictCode(String districtName) async {
+    QuerySnapshot snapshot = await firestore
+        .collection('districts')
+        .where('name', isEqualTo: districtName)
+        .get();
+    return snapshot.docs.isNotEmpty ? snapshot.docs[0]['code'] : null;
+  }
+
+  Future<List<String>> _getWards(String query) async {
+    List<String> wards = [];
+    final addressModel = Provider.of<AddressModel>(context, listen: false);
+    QuerySnapshot snapshot = await firestore
+        .collection('wards')
+        .where('district_code', isEqualTo: addressModel.selectedDistrictCode)
+        .get();
+    for (var doc in snapshot.docs) {
+      wards.add(doc['name']);
+    }
+    return wards;
+  }
+
+  Future<String?> _getWardCode(String wardName) async {
+    QuerySnapshot snapshot = await firestore
+        .collection('wards')
+        .where('name', isEqualTo: wardName)
+        .get();
+    return snapshot.docs.isNotEmpty ? snapshot.docs[0]['code'] : null;
+  }
+
+  final nameController = TextEditingController();
+  final phoneController = TextEditingController();
+  final addressController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is removed from the widget tree.
+    nameController.dispose();
+    phoneController.dispose();
+    addressController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    final addressModel = Provider.of<AddressModel>(context);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Edit Address'),
+        title: const Text('Edit Address'),
       ),
       body: SingleChildScrollView(
-        padding: EdgeInsets.all(16),
+        padding: const EdgeInsets.all(16),
         child: Column(
           children: [
+            CommonTextField(
+              textEditingController: nameController,
+              contentPadding:
+                  const EdgeInsets.only(left: 10, right: 10, top: 5, bottom: 5),
+              hintTextStyle: const TextStyle(color: Colors.grey),
+              focusColor: Colors.black,
+              hintText: 'Name',
+              textFieldPadding:
+                  const EdgeInsets.only(left: 10, right: 10, top: 5, bottom: 5),
+            ),
+            const SizedBox(height: 16),
             TextField(
-              decoration: InputDecoration(
-                labelText: 'Name',
+              controller: phoneController,
+              keyboardType: TextInputType.number,
+              inputFormatters: [
+                FilteringTextInputFormatter.digitsOnly,
+                LengthLimitingTextInputFormatter(10),
+              ],
+              decoration: const InputDecoration(
+                hintText: 'Phone',
               ),
             ),
-            SizedBox(height: 16),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Detail Address',
-              ),
+            const SizedBox(height: 16),
+            CommonTextField(
+              textEditingController: addressController,
+              contentPadding:
+                  const EdgeInsets.only(left: 10, right: 10, top: 5, bottom: 5),
+              hintTextStyle: const TextStyle(color: Colors.grey),
+              focusColor: Colors.black,
+              hintText: 'Address',
+              textFieldPadding:
+                  const EdgeInsets.only(left: 10, right: 10, top: 5, bottom: 5),
             ),
-            SizedBox(height: 16),
+            const SizedBox(height: 16),
             DropdownSearch<String>(
-              items: provinces,
+              asyncItems: _getProvinces,
               popupProps: PopupProps.bottomSheet(
                 fit: FlexFit.loose,
-                bottomSheetProps: BottomSheetProps(
+                bottomSheetProps: const BottomSheetProps(
                   backgroundColor: Colors.white,
                   elevation: 2,
                 ),
@@ -57,21 +168,24 @@ class _EditAddressScreenState extends State<EditAddressScreen> {
                 ),
               ),
               dropdownBuilder: (context, String? item) {
+                addressModel.setSelectedProvinceName(item);
                 return Text(item ?? "Select Province");
               },
-              onChanged: (value) {
-                setState(() {
-                  selectedProvince = value;
-                  // Load districts based on selected province
-                });
+              onChanged: (value) async {
+                if (value != null) {
+                  String? code = await _getProvinceCode(value);
+                  addressModel.setSelectedProvinceCode(code);
+                  addressModel.setSelectedDistrictCode(null);
+                  addressModel.setSelectedWardCode(null);
+                }
               },
             ),
-            SizedBox(height: 16),
+            const SizedBox(height: 16),
             DropdownSearch<String>(
-              items: districts,
+              asyncItems: _getDistricts,
               popupProps: PopupProps.bottomSheet(
                 fit: FlexFit.loose,
-                bottomSheetProps: BottomSheetProps(
+                bottomSheetProps: const BottomSheetProps(
                   backgroundColor: Colors.white,
                   elevation: 2,
                 ),
@@ -80,21 +194,27 @@ class _EditAddressScreenState extends State<EditAddressScreen> {
                 ),
               ),
               dropdownBuilder: (context, String? item) {
-                return Text(item ?? "Select District");
+                if (addressModel.selectedDistrictCode == null) {
+                  return const Text("Select District");
+                } else {
+                  addressModel.setSelectedDistrictName(item);
+                  return Text(item ?? "Select District");
+                }
               },
-              onChanged: (value) {
-                setState(() {
-                  selectedDistrict = value;
-                  // Load wards based on selected district
-                });
+              onChanged: (value) async {
+                if (value != null) {
+                  String? code = await _getDistrictCode(value);
+                  addressModel.setSelectedDistrictCode(code);
+                  addressModel.setSelectedWardCode(null);
+                }
               },
             ),
-            SizedBox(height: 16),
+            const SizedBox(height: 16),
             DropdownSearch<String>(
-              items: wards,
+              asyncItems: _getWards,
               popupProps: PopupProps.bottomSheet(
                 fit: FlexFit.loose,
-                bottomSheetProps: BottomSheetProps(
+                bottomSheetProps: const BottomSheetProps(
                   backgroundColor: Colors.white,
                   elevation: 2,
                 ),
@@ -103,29 +223,82 @@ class _EditAddressScreenState extends State<EditAddressScreen> {
                 ),
               ),
               dropdownBuilder: (context, String? item) {
-                return Text(item ?? "Select Ward");
+                if (addressModel.selectedWardCode == null) {
+                  return const Text("Select Ward");
+                } else {
+                  addressModel.setSelectedWardName(item);
+                  return Text(item ?? "Select Ward");
+                }
               },
-              onChanged: (value) {
-                setState(() {
-                  selectedWard = value;
-                });
+              onChanged: (value) async {
+                if (value != null) {
+                  String? code = await _getWardCode(value);
+                  addressModel.setSelectedWardCode(code);
+                }
               },
             ),
-            SizedBox(height: 16),
-            ElevatedButton(
-              child: Text('Save'),
-              onPressed: () {
-                // Save address logic
+            const SizedBox(height: 16),
+            CommonButton(
+              buttonText: 'Save',
+              onTap: () {
+                if (nameController.text.isEmpty ||
+                    phoneController.text.isEmpty ||
+                    addressController.text.isEmpty) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('Please fill in all fields'),
+                    ),
+                  );
+                  return;
+                }
+
+                if (addressModel.selectedProvinceCode == null ||
+                    addressModel.selectedDistrictCode == null ||
+                    addressModel.selectedWardCode == null) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content:
+                          Text('Please select province, district, and ward'),
+                    ),
+                  );
+                  return;
+                }
+                addressModel.setName(nameController.text);
+                addressModel.setPhone(phoneController.text);
+                addressModel.setAddress(addressController.text);
+                _updateAddress(addressModel, 'address_id');
+                Navigator.pop(context);
               },
-            ),
-            SizedBox(height: 16),
-            ElevatedButton(
-              child: Text('Use your location'),
-              onPressed: _useCurrentLocation,
             ),
           ],
         ),
       ),
     );
+  }
+
+  void _updateAddress(AddressModel addressModel, String documentId) async {
+    FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+    try {
+      await firestore.collection('addresses').doc(documentId).update({
+        'name': addressModel.name,
+        'phone': addressModel.phone,
+        'address': addressModel.address,
+        'province': addressModel.selectedProvinceName,
+        'district': addressModel.selectedDistrictName,
+        'ward': addressModel.selectedWardName,
+      });
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Address updated successfully'),
+        ),
+      );
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Failed to update address'),
+        ),
+      );
+    }
   }
 }
