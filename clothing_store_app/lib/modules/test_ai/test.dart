3ffa6bfe2bb706ca84f5e3eef96d6614d01c68eb
zHuskyz <nvk01052004@gmail.com>
2024-08-29 10:27:37 +0700

    finish ai

--- clothing_store_app/lib/modules/test_ai/test.dart
+++ clothing_store_app/lib/modules/test_ai/test.dart
@@ -1,11 +1,6 @@
-import 'dart:io';
-import 'dart:typed_data';
-import 'package:clothing_store_app/modules/test_ai/result.dart';
-import 'package:tflite_flutter/tflite_flutter.dart' as tfl;
 import 'package:flutter/material.dart';
+import 'package:flutter_tflite/flutter_tflite.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:image/image.dart' as img;
-import 'dart:math';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -14,22 +9,29 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  late File _image;
-  dynamic _probability = 0;
-  String? _result;
-  List<String>? _labels;
-  tfl.Interpreter? _interpreter;
   final picker = ImagePicker();
+  late List _recognitions;
+
+  initTFlite() async {
+    await Tflite.loadModel(
+      model: "assets/model_unquant.tflite",
+      labels: "assets/labels.txt",
+      isAsset: true,
+      numThreads: 1,
+      useGpuDelegate: false,
+    );
+  }
+
   @override
   void initState() {
     super.initState();
-    loadModel().then((_) {
-      loadLabels().then((loadedLabels) {
-        setState(() {
-          _labels = loadedLabels;
-        });
-      });
-    });
+    initTFlite();
+  }
+
+  @override
+  void dispose() {
+    Tflite.close();
+    super.dispose();
   }
 
   @override
@@ -85,106 +87,33 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  void navigateToResult() {
-    Navigator.push(
-        context,
-        MaterialPageRoute(
-            builder: (context) => ResultPage(
-                  resultImage: _image,
-                  result: _result!,
-                  probability: _probability,
-                )));
-  }
-
-  Future<void> loadModel() async {
-    try {
-      _interpreter =
-          await tfl.Interpreter.fromAsset('assets/model_unquant.tflite');
-    } catch (e) {
-      debugPrint('Error loading model: $e');
-    }
-  }
-
   Future<void> pickImageFromCamera() async {
     final pickedFile = await picker.pickImage(source: ImageSource.camera);
     if (pickedFile != null) {
-      _setImage(File(pickedFile.path));
+      recognizeImage(pickedFile);
     }
   }
 
   Future<void> pickImageFromGallery() async {
     final pickedFile = await picker.pickImage(source: ImageSource.gallery);
     if (pickedFile != null) {
-      _setImage(File(pickedFile.path));
+      recognizeImage(pickedFile);
     }
   }
 
-  void _setImage(File image) {
+  Future recognizeImage(XFile image) async {
+    int startTime = new DateTime.now().millisecondsSinceEpoch;
+    var recognitions = await Tflite.runModelOnImage(
+      path: image.path,
+      numResults: 6,
+      threshold: 0.05,
+      imageMean: 127.5,
+      imageStd: 127.5,
+    );
     setState(() {
-      _image = image;
+      _recognitions = recognitions!;
     });
-    runInference();
-  }
-
-  Future<Uint8List> preprocessImage(File imageFile) async {
-    // Decode the image to an Image object
-    img.Image? originalImage = img.decodeImage(await imageFile.readAsBytes());
-
-    // Resize the image to the correct size
-    img.Image resizedImage =
-        img.copyResize(originalImage!, width: 256, height: 256);
-
-    // Convert to a byte buffer in the format suitable for TensorFlow Lite (RGB)
-    // The model expects a 4D tensor [1, 224, 224, 3]
-    // Flatten the resized image to match this shape
-    Uint8List bytes = resizedImage.getBytes();
-    return bytes;
-  }
-
-  Future<void> runInference() async {
-    if (_labels == null) {
-      return;
-    }
-
-    try {
-      Uint8List inputBytes = await preprocessImage(_image);
-      var input = inputBytes.buffer.asUint8List().reshape([1, 256, 256, 3]);
-      var outputBuffer = List<int>.filled(1 * 4, 0).reshape([1, 4]);
-
-      _interpreter!.run(input, outputBuffer);
-
-      // Assuming output is now List<List<int>> after inference
-      List<double> output = outputBuffer[0];
-
-      // Print raw output for debugging
-      debugPrint('Raw output: $output');
-
-      // Calculate probability
-      double maxScore = output.reduce(max);
-      _probability = (maxScore / 255.0); // Convert to percentage
-
-      // Get the classification result
-      int highestProbIndex = output.indexOf(maxScore);
-      String classificationResult = _labels![highestProbIndex];
-
-      setState(() {
-        _result = classificationResult;
-        // _probability is updated with the calculated probability
-      });
-      navigateToResult();
-    } catch (e) {
-      debugPrint('Error during inference: $e');
-    }
-  }
-
-  Future<List<String>> loadLabels() async {
-    final labelsData =
-        await DefaultAssetBundle.of(context).loadString('assets/labels.txt');
-    return labelsData.split('\n');
-  }
-
-  String classifyImage(List<int> output) {
-    int highestProbIndex = output.indexOf(output.reduce(max));
-    return _labels![highestProbIndex];
+    int endTime = new DateTime.now().millisecondsSinceEpoch;
+    print("Inference took ${endTime - startTime}ms. Result is $_recognitions");
   }
 }
